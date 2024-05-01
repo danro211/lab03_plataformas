1f4b3071ea73a20741992cbbb102259a008cd95f
Daniel Rojas <daniel.rojascanas@ucr.ac.cr>
2024-05-01 02:29:13 -0600

    Terminacion ejercicio3

--- ejercicio3/ej3.c
+++ ejercicio3/ej3.c
@@ -1,22 +1,80 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-#define SIZE 5
+#define MAX_SIZE 10  // Define el tamano maximo para la matriz
 
-int findLargestSquare(int matrix[][SIZE]) {
-    // Su implementacion
+// Funcion para encontrar el minimo de tres valores
+int min(int a, int b, int c) {
+    int m = a;
+    if (b < m) m = b;
+    if (c < m) m = c;
+    return m;
+}
+
+// Funcion para encontrar el tamano del cuadrado mas grande de unos
+int findLargestSquare(int matrix[][MAX_SIZE], int size) {
+    int s[size][size]; // Matriz para almacenar los resultados parciales
+    int max = 0;  // Variable para rastrear el mayor tamano encontrado
+
+    // Construir la matriz de soluciones parciales
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            if (i == 0 || j == 0) {
+                s[i][j] = matrix[i][j]; // Caso base: primera fila o columna
+            } else if (matrix[i][j] == 1) {
+                s[i][j] = min(s[i][j-1], s[i-1][j], s[i-1][j-1]) + 1;
+            } else {
+                s[i][j] = 0;
+            }
+            if (max < s[i][j]) max = s[i][j]; // Actualizar max si es necesario
+        }
+    }
+
+    return max;
+}
+
+// Funcion para llenar la matriz con valores aleatorios 0 o 1
+void fillMatrix(int matrix[][MAX_SIZE], int size) {
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            matrix[i][j] = rand() % 2;  // Generar 0 o 1 aleatoriamente
+        }
+    }
+}
+
+// Funcion para imprimir la matriz
+void printMatrix(int matrix[][MAX_SIZE], int size) {
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            printf("%d ", matrix[i][j]); // Imprimir cada elemento de la matriz
+        }
+        printf("\n");
+    }
 }
 
 int main() {
-    int matrix[SIZE][SIZE] = {
-        {0, 1, 1, 1, 0},
-        {0, 1, 1, 1, 1},
-        {1, 1, 1, 1, 0},
-        {0, 1, 1, 0, 1},
-        {1, 0, 1, 0, 1}
-    };
+    srand(time(NULL));  // Inicializar la semilla para la funcion rand
 
-    int largestSquareSize = findLargestSquare(matrix);
-    printf("El tamaño del cuadrado más grande de unos es: %d\n", largestSquareSize);
+    int n;
+    printf("Ingrese el tamano de la matriz nxn (1-10): ");
+    scanf("%d", &n);  // Leer el tamano de la matriz del usuario
 
-    return 0;
+    // Comprobar la validez del tamano ingresado
+    if (n < 1 || n > 10) {
+        printf("Numero invalido. Usando tamano por defecto de 5.\n");
+        n = 5;  // Usar valor por defecto si el tamano no es valido
+    }
+
+    int matrix[MAX_SIZE][MAX_SIZE]; // Declarar la matriz
+
+    fillMatrix(matrix, n); // Llenar la matriz con valores aleatorios
+
+    printf("La matriz utilizada corresponde a:\n");
+    printMatrix(matrix, n); // Imprimir la matriz
+
+    int largestSquareSize = findLargestSquare(matrix, n); // Calcular el cuadrado mas grande
+    printf("El tamano del cuadrado mas grande de unos es: %d\n", largestSquareSize);
+
+    return 0; // Finalizar el programa
 }
