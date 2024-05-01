3b515e2cc9a856530c40e78c0f25d0a0ba073684
Daniel Rojas <daniel.rojascanas@ucr.ac.cr>
2024-05-01 00:24:10 -0600

    correccion codigo dado

--- ejercicio2/ej2.c
+++ ejercicio2/ej2.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
-int factorial (int n) {
+int factorial(int n) {
     int i = 1;
     while (n > 1) {
         i = i * n;
-        int n = n - 1;
+        n = n - 1;  // Corregido: Eliminada la declaracion 'int', ahora se decrementa 'n' directamente
     }
     return i;
 }
 
-int main (int argc, char *argv[]) {
+int main(int argc, char *argv[]) {
     int fac4 = factorial(4);
     int fac5 = factorial(5);
     printf("4! = %d, 5! = %d\n", fac4, fac5);
